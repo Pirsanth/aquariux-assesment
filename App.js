7eb80ccdf1419c61aaf92abfaffc05dd1c66427b
Pirsanth <pirsanth@hotmail>
2022-04-02 16:24:07 +0800

    basic styling for first screen done

--- App.js
+++ App.js
@@ -16,17 +16,72 @@ import {
   Text,
   useColorScheme,
   View,
+  Image,
+  TouchableOpacity
 } from 'react-native';
+import {LIGHT_GREY, SLIGHTLY_DARKER_GREY} from './colors';
 
+const SPACE_FROM_EDGE = 20;
 
 const App = () => {
 
 
+  //If you're wondering why there's a View, another View and a SafeAreaView its so the gray header extends up to the iphone's notch
   return (
-    <SafeAreaView style={{flex:1, backgroundColor:'orange'}}>
+    <View style={{flex:1}}>
       
-      
-    </SafeAreaView>
+      <View style={{
+        backgroundColor:LIGHT_GREY,
+        paddingBottom:10,
+        borderBottomWidth:1,
+        borderBottomColor:SLIGHTLY_DARKER_GREY
+      }}>
+        <SafeAreaView
+          style={{
+            flexDirection:'row',
+            justifyContent:'space-between',
+            alignItems:'center',
+            marginTop:20,
+            marginHorizontal:SPACE_FROM_EDGE
+          }}
+        >
+          <TouchableOpacity style={{
+          }}>
+            <Image 
+              style={{
+                height:25,
+                width:25
+              }}
+              source={require('./searchIcon.png')}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+
+          <Text style={{
+            fontWeight:'bold',
+            fontSize:18
+          }}>Contacts</Text>
+
+          <TouchableOpacity style={{
+          }}>
+            <Image 
+              style={{
+                height:25,
+                width:25
+              }}
+              source={require('./plusIcon.png')}
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        </SafeAreaView>
+      </View>
+
+      <SafeAreaView style={{
+        flex:1,
+      }}>
+          
+      </SafeAreaView>
+    </View>
   );
 };
 
---
+++ colors.js
@@ -0,0 +1,2 @@
+export const LIGHT_GREY = '#F6F6F6';
+export const SLIGHTLY_DARKER_GREY = '#E5E5E5';
